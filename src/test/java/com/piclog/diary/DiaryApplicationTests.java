879519775836e942cd7a6c03afcc478375a8029b
colon <semicc34@gmail.com>
2026-03-23 22:27:04 +0900

    Initial commit: Piclog Diary project

---
+++ src/test/java/com/piclog/diary/DiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piclog.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
