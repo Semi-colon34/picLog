69f13dba3871e47bd2edacdf1172f587fe733056
colon <semicc34@gmail.com>
2026-05-24 16:00:03 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/piclog/piclog/PiclogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piclog.piclog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiclogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
